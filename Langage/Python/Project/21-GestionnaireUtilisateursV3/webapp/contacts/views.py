22c7dce5c33319d0dc9003f6951fd7ea88a34669
domino659 <martin@sionfamily.com>
2022-05-23 18:56:44 +0200

    21

---
+++ Langage/Python/Project/21-GestionnaireUtilisateursV3/webapp/contacts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+
+from api.crm import get_all_users, User
+
+
+def index(request):
+    return render(request, 'contacts/index.html', {'users': get_all_users()})
+
+def add_contact(request):
+    first_name = request.POST.get("first_name")
+    last_name = request.POST.get("last_name")
+    phone_number = request.POST.get("phone_number")
+    address = request.POST.get("address")
+
+    user = User(first_name, last_name, phone_number, address)
+    user.save()
+
+    return redirect('index')
