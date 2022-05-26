86178ae4ac681e1957d49c0ecfc31d93d3078358
domino659 <martin@sionfamily.com>
2022-05-26 12:11:07 +0200

    fix bot

--- Langage/Python/Project/23-BotDiscord/shrine/main.py
+++ Langage/Python/Project/23-BotDiscord/shrine/main.py
@@ -1,25 +1,14 @@
 import os
+import discord
+from discord.ext import commands
 
 from utils import env
 from ShrineBot import ShrineBot
 
-import discord
-from discord.ext import commands
-
-class Say(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def Say (self,ctx,*,arg):
-
-        def get_message(self, ctx, message):
-            message = ctx.message.content.split("!say", " ")
-            
-            return message
-
-        await ctx.send(message)
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 if __name__ == '__main__':
-    shrine_bot = ShrineBot()
+    shrine_bot = ShrineBot(bot)
     shrine_bot.run(os.getenv("TOKEN"))
